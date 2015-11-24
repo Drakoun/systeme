a7c092ab35cae274c724c5293f8d842c4aadfb80
Drakoun <davidboccara333@yahoo.fr>
2015-11-24 10:15:45 +0100

    Correction diverses pour la création master et slave. Ajout de la création de l'abre binaire complet.

--- src/main/java/fr/unice/miage/sd/tinydfs/main/MasterMain.java
+++ src/main/java/fr/unice/miage/sd/tinydfs/main/MasterMain.java
@@ -5,6 +5,7 @@ import fr.unice.miage.sd.tinydfs.nodes.Slave;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.rmi.AlreadyBoundException;
+import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
@@ -18,10 +19,11 @@ public class MasterMain extends UnicastRemoteObject implements Master {
     private int nbSlaves;
     private Slave leftSlave;
     private Slave rightSlave;
+    private static final Object lock = new Object();
     
     // Usage: java fr.unice.miage.sd.tinydfs.main.MasterMain storage_service_name dfs_root_folder nb_slaves
     public static void main(String[] args) throws RemoteException,
-                AlreadyBoundException, NotBoundException, MalformedURLException {
+                AlreadyBoundException, NotBoundException, MalformedURLException, InterruptedException {
             String storageServiceName = args[0];
             String dfsRootFolder = args[1];
             int nbSlaves = Integer.parseInt(args[2]);
@@ -33,22 +35,70 @@ public class MasterMain extends UnicastRemoteObject implements Master {
             
             MasterMain masterMain = new MasterMain(dfsRootFolder,nbSlaves);
             registry.bind(storageServiceName, masterMain);
-            
+            /*
+            // A retirer avant le rendu, executer par le code python
             String[] param = new String[3];
-            param[0] = storageServiceName;
+            param[0] = "localhost";
             param[1] = ".";
             System.out.println("Master : Création des " + nbSlaves + " slaves");
-            for (int i = 2; i < nbSlaves+2; i++) {
+            for (int i = 0; i < nbSlaves; i++) {
                 param[2] = i + "";
                 SlaveMain.main(param);
+            }*/
+            
+            System.out.println("Master : En attente des slaves");
+            boolean slavePret = false;
+            while (!slavePret) {
+                Thread.sleep(2000);
+                slavePret = true;
+                for (int i = 2; i < nbSlaves+2; i++) {
+                    try {
+                        Slave slave = (Slave) Naming.lookup("rmi://localhost/" + "Slave" +i);
+                    }
+                    catch (NotBoundException ex) {
+                        slavePret = false;
+                        break;
+                    }
+                }
             }
             
+            System.out.println("Master : Tous les slaves sont créés. Création de l'arbre binaire complet");
+            // Verification puis creation de l'arbre binaire complet
+            if (nbSlaves > 1 && (Long.bitCount(nbSlaves+2) == 1)) {
+                //Slave gauche = (Slave) Naming.lookup("rmi://localhost/" + "Slave2");
+                masterMain.setLeftSlave((Slave) Naming.lookup("rmi://localhost/" + "Slave2"));
+                masterMain.setRightSlave((Slave) Naming.lookup("rmi://localhost/" + "Slave3"));
+                System.out.println("Master : fils gauche et fils droit attribués");
+                //System.out.println("Master : fils gauche : " + gauche.getId());
+                
+                int i = 2;
+                while ((2 * i) < nbSlaves) {
+                    Slave filsGauche = (Slave) Naming.lookup("rmi://localhost/" + "Slave" + (2*i));
+                    Slave filsDroit = (Slave) Naming.lookup("rmi://localhost/" + "Slave" + ((2*i) + 1));
+                    Slave noeud = (Slave) Naming.lookup("rmi://localhost/" + "Slave" + i);
+                    noeud.setLeftSlave(filsGauche);
+                    noeud.setRightSlave(filsDroit);
+                    i++;
+                }
+                System.out.println("Master : Création de l'arbe binaire complet réussi.");
+            }
+            else {
+                System.out.println("Master : Création de l'arbre binaire complet échoué. Le nombre de Slave ne permet pas de le faire.");
+            }
     }
 
     public MasterMain(String dfsRootFolder, int nbSlaves) throws RemoteException {
         super();
         this.dfsRootFolder = dfsRootFolder;
         this.nbSlaves = nbSlaves;
+    }
+    
+    private void setLeftSlave(Slave slave) {
+        this.leftSlave = slave;
+    }
+    
+    private void setRightSlave(Slave slave) {
+        this.rightSlave = slave;
     }
     
     @Override
@@ -60,7 +110,7 @@ public class MasterMain extends UnicastRemoteObject implements Master {
     public int getNbSlaves() throws RemoteException {
         return this.nbSlaves;
     }
-
+    
     @Override
     public void saveFile(File file) throws RemoteException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
--- src/main/java/fr/unice/miage/sd/tinydfs/main/SlaveMain.java
+++ src/main/java/fr/unice/miage/sd/tinydfs/main/SlaveMain.java
@@ -1,11 +1,12 @@
 package fr.unice.miage.sd.tinydfs.main;
 
-import fr.unice.miage.sd.tinydfs.nodes.Master;
 import fr.unice.miage.sd.tinydfs.nodes.Slave;
 import java.net.MalformedURLException;
+import java.rmi.AlreadyBoundException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.List;
 
@@ -19,21 +20,23 @@ public class SlaveMain extends UnicastRemoteObject implements Slave {
     
     // Usage: java fr.unice.miage.sd.tinydfs.main.SlaveMain master_host dfs_root_folder slave_identifier
     public static void main(String[] args) throws RemoteException,
-                NotBoundException, MalformedURLException {
+                NotBoundException, MalformedURLException, AlreadyBoundException {
             String masterHost = args[0];
             String dfsRootFolder = args[1];
             int slaveId = Integer.parseInt(args[2]);
 
             // Create slave and register it (registration name must be "slave" + slave identifier)
             SlaveMain slaveMain = new SlaveMain(slaveId, dfsRootFolder);
-            System.out.println("Slave" + slaveId + " : Créé");
-            Master slaveMainStub = (Master) Naming.lookup("rmi://localhost/" + masterHost);
+            System.out.println("Slave" + slaveMain.getId() + " : Créé");
+            Registry registry = (Registry) Naming.lookup("rmi://" + masterHost);
+            registry.bind("Slave" + slaveMain.getId(), slaveMain);
+            System.out.println("Slave" + slaveMain.getId() + " : Enregistré dans le RMI registry");
             
     }
 
     public SlaveMain(int id, String dfsRootFolder) throws RemoteException {
         super();
-        this.id = id;
+        this.id = id + 2;
         this.dfsRootFolder = dfsRootFolder;
     }
     
